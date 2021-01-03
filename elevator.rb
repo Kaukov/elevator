4f842bc40834e8ee2a0505905e14c2d9f2416fa2
Peter Kaukov <peter@kaukov.dev>
2021-01-03 11:50:17 +0200

    Fix code style by RuboCop

--- elevator.rb
+++ elevator.rb
@@ -1,14 +1,18 @@
+# frozen_string_literal: true
+
 require 'pry'
 
+# Elevator class
 class Elevator
-  def initialize(queues, capacity, debug = false)
+  def initialize(queues, capacity, debug: false)
+    # Init
   end
 
   def process
+    # Process all queues
   end
 
   def history
     []
   end
 end
-
--- elevator_spec.rb
+++ elevator_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'rspec'
 require 'pry'
 require_relative 'elevator'
@@ -6,24 +8,24 @@ describe Elevator do
   let(:capacity) { 5 }
 
   [
-    {queues: [ [], [], [5,5,5], [], [], [], [] ],
-     answer: [0, 2, 5, 0],
-     debug: false},
-    {queues: [ [], [], [1,1], [], [], [], [] ],
-     answer: [0, 2, 1, 0],
-     debug: false},
-    {queues: [ [], [3], [4], [], [5], [], [] ],
-     answer: [0, 1, 2, 3, 4, 5, 0],
-     debug: false},
-    {queues: [ [], [0], [], [], [2], [3], [] ],
-     answer: [0, 5, 4, 3, 2, 1, 0],
-     debug: false},
-    {queues: [ [], [0,3,3,5,5,5], [], [0,0,2,2,5,5,5], [2], [3], [0,0,0,4,4,4] ],
-     answer: [0, 1, 3, 5, 6, 5, 4, 3, 2, 1, 0, 3, 5, 6, 5, 4, 3, 2, 0],
-     debug: false},
+    { queues: [[], [], [5, 5, 5], [], [], [], []],
+      answer: [0, 2, 5, 0],
+      debug: false },
+    { queues: [[], [], [1, 1], [], [], [], []],
+      answer: [0, 2, 1, 0],
+      debug: false },
+    { queues: [[], [3], [4], [], [5], [], []],
+      answer: [0, 1, 2, 3, 4, 5, 0],
+      debug: false },
+    { queues: [[], [0], [], [], [2], [3], []],
+      answer: [0, 5, 4, 3, 2, 1, 0],
+      debug: false },
+    { queues: [[], [0, 3, 3, 5, 5, 5], [], [0, 0, 2, 2, 5, 5, 5], [2], [3], [0, 0, 0, 4, 4, 4]],
+      answer: [0, 1, 3, 5, 6, 5, 4, 3, 2, 1, 0, 3, 5, 6, 5, 4, 3, 2, 0],
+      debug: false }
   ].each do |example|
     it "solves #{example[:queues]} with #{example[:answer]}" do
-      e = Elevator.new example[:queues], capacity, example[:debug]
+      e = Elevator.new example[:queues], capacity, debug: example[:debug]
       e.process
 
       e.history.should eq example[:answer]
@@ -36,4 +38,3 @@ RSpec.configure do |config|
     c.syntax = :should
   end
 end
-
