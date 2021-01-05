5b1fa17f53d8f401921941290ca14a50ef2d258b
Peter Kaukov <peter@kaukov.dev>
2021-01-05 23:14:01 +0200

    Begin rewriting the elevator algorithm
    Currently only the last test fails

--- elevator.rb
+++ elevator.rb
@@ -4,15 +4,94 @@ require 'pry'
 
 # Elevator
 class Elevator
+  attr_reader :history
+
   def initialize(queues, capacity, debug: false)
+    @current_floor = 0
     @history = [0]
+    @queues = hash_queues queues
+    @capacity = capacity
+    @debug = debug
+    @elevator = []
+
+    @direction = 1
+
+    @last_floor = queues.length - 1
   end
 
   def process
-    #
+    # Go up or down, change direction, skip floors there's no one queued for/on
+
+    until @queues.keys.length.zero? && @elevator.length.zero?
+      process_current_floor
+
+      change_floor
+
+      check_for_direction_change
+    end
+
+    add_historical_floor 0
   end
 
-  def history
-    @history
+  private
+
+  def hash_queues(queues)
+    queues_hash = {}
+
+    queues.each_with_index do |queue, floor|
+      queues_hash[floor.to_s] = queue if queue.length.positive?
+    end
+
+    queues_hash
+  end
+
+  def process_current_floor
+    # Delete each person in the elevator queued for the current floor
+    @elevator.each do |item|
+      next unless item.abs == @current_floor
+
+      @elevator.delete item
+
+      add_historical_floor @current_floor
+    end
+
+    return unless @queues[@current_floor.to_s]
+
+    deleted_items_from_queue = 0
+    queue_items_delete = []
+
+    @queues[@current_floor.to_s].each_with_index do |item, index|
+      break unless @elevator.length <= @capacity
+
+      next unless item.abs > @current_floor && @direction == 1 || item.abs < @current_floor && @direction.zero?
+
+      add_historical_floor @current_floor
+
+      @elevator << item
+
+      # @queues[@current_floor.to_s].delete_at index - deleted_items_from_queue
+
+      # deleted_items_from_queue += 1
+
+      queue_items_delete << index
+    end
+
+    queue_items_delete.reverse.each { |index| @queues[@current_floor.to_s].delete_at index }
+
+    @queues.delete @current_floor.to_s unless @queues[@current_floor.to_s].length.positive?
+  end
+
+  def change_floor
+    @current_floor += 1 if @direction.positive?
+    @current_floor -= 1 if @direction.zero?
+  end
+
+  def check_for_direction_change
+    @direction = 0 if @current_floor == @last_floor
+    @direction = 1 if @current_floor.zero?
+  end
+
+  def add_historical_floor(floor)
+    @history.push floor unless @history.last.eql? floor
   end
 end
